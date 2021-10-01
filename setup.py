2890d7593efcb5c527b46731f70f2b32b6f02999
Ryan Wood <ryanwood1193@gmail.com>
2021-10-01 16:16:05 +0900

    Removed bad commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author = "Alphaus KK",
     description = "Alphaus KK's Blue API for Python",
     url = "https://github.com/alphauslabs/blue-sdk-python",
-    version = "0.2.1",
+    version = "0.3.0",
     packages = find_namespace_packages(),
     options = {
         "bdist_wheel": {
