0b8bd802462d3de2f6d477d62257b5ed91e68125
Ryan Wood <ryanwood1193@gmail.com>
2021-10-01 20:45:49 +0900

    Added session and connection code; still some more work to do here

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author = "Alphaus KK",
     description = "Alphaus KK's Blue API for Python",
     url = "https://github.com/alphauslabs/blue-sdk-python",
-    version = "0.3.0",
+    version = "0.4.0",
     packages = find_namespace_packages(),
     options = {
         "bdist_wheel": {
