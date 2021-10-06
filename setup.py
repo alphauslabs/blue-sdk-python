67414fca66cdc9b2161e061a5b6ccb3956be36a1
Ryan Wood <ryanwood1193@gmail.com>
2021-10-06 15:29:02 +0900

    Added author email to setup.py; added pyproject.toml to manifest

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_namespace_packages, setup
 setup(
     name = "alphausblue",
     author = "Alphaus KK",
+    author_email = "dev@alphaus.cloud",
     description = "Alphaus KK's Blue API for Python",
     url = "https://github.com/alphauslabs/blue-sdk-python",
     version = "",
