da79b79529e6ab041886ba3035644f1613521e3f
mobingideployer <dev@mobingi.com>
2022-02-22 03:28:15 +0000

    refs/heads/main c006f419036472ac5f9373f124c029e374cdd608

--- alphausblue/billing/v1/billing_pb2_grpc.py
+++ alphausblue/billing/v1/billing_pb2_grpc.py
@@ -80,7 +80,7 @@ class BillingServicer(object):
         raise NotImplementedError('Method not implemented!')
 
     def ListAwsDailyRunHistory(self, request, context):
-        """WORK-IN-PROGRESS: Reads the calculation history of all accounts in your billing groups. Only available in Ripple.
+        """Reads the daily calculation history of all accounts in your billing groups. Only available in Ripple.
         """
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
