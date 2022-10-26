15e0907f8ef559379112e361f9c58311652b1b68
mobingideployer <dev@mobingi.com>
2022-10-26 02:29:17 +0000

    refs/heads/main d159b8e782b286299b7d5b9ef2d8f23421862ea9

--- alphausblue/api/cover/rightsizingrecommendation_pb2.py
+++ alphausblue/api/cover/rightsizingrecommendation_pb2.py
@@ -11,10 +11,9 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from alphausblue.api.cover import user_pb2 as api_dot_cover_dot_user__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n)api/cover/rightsizingrecommendation.proto\x12\x11\x62lueapi.api.cover\x1a\x14\x61pi/cover/user.proto\"S\n\x0b\x41\x63\x63ountData\x12\x11\n\taccountId\x18\x01 \x01(\t\x12\x31\n\x08resource\x18\x02 \x03(\x0b\x32\x1f.blueapi.api.cover.ResourceData\"\xde\x02\n\x0cResourceData\x12\x12\n\nresourceId\x18\x01 \x01(\t\x12\x14\n\x0cresourceName\x18\x02 \x01(\t\x12\x14\n\x0cresourceType\x18\x03 \x01(\t\x12\x17\n\x0f\x63onsumedService\x18\x04 \x01(\t\x12\x14\n\x0c\x63urrentPrice\x18\x05 \x01(\x01\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x19\n\x11maxCpuUtilization\x18\x07 \x01(\x01\x12\x1d\n\x15maxStorageUtilization\x18\x08 \x01(\x01\x12\x1c\n\x14maxMemoryUtilization\x18\t \x01(\x01\x12\x17\n\x0fnetworkCapacity\x18\n \x01(\t\x12\x17\n\x0frightSizingType\x18\x0b \x01(\t\x12\x45\n\x14recommendationDetail\x18\x0c \x03(\x0b\x32\'.blueapi.api.cover.RecommendationDetail\"x\n\x14RecommendationDetail\x12\x1f\n\x17recommendedResourceType\x18\x01 \x01(\t\x12\x15\n\restimatedCost\x18\x02 \x01(\x01\x12\x18\n\x10\x65stimatedSavings\x18\x03 \x01(\x01\x12\x0e\n\x06region\x18\x04 \x01(\tBw\n\x1f\x63loud.alphaus.blueapi.api.coverB&ApiCoverRightSizingRecommendationProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n)api/cover/rightsizingrecommendation.proto\x12\x11\x62lueapi.api.cover\"S\n\x0b\x41\x63\x63ountData\x12\x11\n\taccountId\x18\x01 \x01(\t\x12\x31\n\x08resource\x18\x02 \x03(\x0b\x32\x1f.blueapi.api.cover.ResourceData\"\xde\x02\n\x0cResourceData\x12\x12\n\nresourceId\x18\x01 \x01(\t\x12\x14\n\x0cresourceName\x18\x02 \x01(\t\x12\x14\n\x0cresourceType\x18\x03 \x01(\t\x12\x17\n\x0f\x63onsumedService\x18\x04 \x01(\t\x12\x14\n\x0c\x63urrentPrice\x18\x05 \x01(\x01\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x19\n\x11maxCpuUtilization\x18\x07 \x01(\x01\x12\x1d\n\x15maxStorageUtilization\x18\x08 \x01(\x01\x12\x1c\n\x14maxMemoryUtilization\x18\t \x01(\x01\x12\x17\n\x0fnetworkCapacity\x18\n \x01(\t\x12\x17\n\x0frightSizingType\x18\x0b \x01(\t\x12\x45\n\x14recommendationDetail\x18\x0c \x03(\x0b\x32\'.blueapi.api.cover.RecommendationDetail\"x\n\x14RecommendationDetail\x12\x1f\n\x17recommendedResourceType\x18\x01 \x01(\t\x12\x15\n\restimatedCost\x18\x02 \x01(\x01\x12\x18\n\x10\x65stimatedSavings\x18\x03 \x01(\x01\x12\x0e\n\x06region\x18\x04 \x01(\tBw\n\x1f\x63loud.alphaus.blueapi.api.coverB&ApiCoverRightSizingRecommendationProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'api.cover.rightsizingrecommendation_pb2', globals())
@@ -22,10 +21,10 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'\n\037cloud.alphaus.blueapi.api.coverB&ApiCoverRightSizingRecommendationProtoZ,github.com/alphauslabs/blue-sdk-go/api/cover'
-  _ACCOUNTDATA._serialized_start=86
-  _ACCOUNTDATA._serialized_end=169
-  _RESOURCEDATA._serialized_start=172
-  _RESOURCEDATA._serialized_end=522
-  _RECOMMENDATIONDETAIL._serialized_start=524
-  _RECOMMENDATIONDETAIL._serialized_end=644
+  _ACCOUNTDATA._serialized_start=64
+  _ACCOUNTDATA._serialized_end=147
+  _RESOURCEDATA._serialized_start=150
+  _RESOURCEDATA._serialized_end=500
+  _RECOMMENDATIONDETAIL._serialized_start=502
+  _RECOMMENDATIONDETAIL._serialized_end=622
 # @@protoc_insertion_point(module_scope)
