06c7e5d897585380783c3592fc96422865f52598
mobingideployer <dev@mobingi.com>
2022-11-11 07:22:43 +0000

    refs/heads/main 206a0ade5ae2b2c49ac34c3763de7ade94211e18

--- alphausblue/api/cover/costforecast_pb2.py
+++ alphausblue/api/cover/costforecast_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1c\x61pi/cover/costforecast.proto\x12\x11\x62lueapi.api.cover\"\xcc\x01\n\x0f\x41wsCostForecast\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0f\n\x07\x61\x63\x63ount\x18\x02 \x01(\t\x12\x13\n\x0bproductCode\x18\x03 \x01(\t\x12\x0c\n\x04\x63ost\x18\x04 \x01(\x02\x12\x12\n\nlowerBound\x18\x05 \x01(\x02\x12\x12\n\nupperBound\x18\x06 \x01(\x02\x12\x15\n\runblendedCost\x18\x07 \x01(\x02\x12\x1b\n\x13unblendedLowerBound\x18\x08 \x01(\x02\x12\x1b\n\x13unblendedUpperBound\x18\t \x01(\x02\x42j\n\x1f\x63loud.alphaus.blueapi.api.coverB\x19\x41piCoverCostForecastProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1c\x61pi/cover/costforecast.proto\x12\x11\x62lueapi.api.cover\"\xcc\x01\n\x0f\x41wsCostForecast\x12\x0c\n\x04\x64\x61te\x18\x01 \x01(\t\x12\x0f\n\x07\x61\x63\x63ount\x18\x02 \x01(\t\x12\x13\n\x0bproductCode\x18\x03 \x01(\t\x12\x0c\n\x04\x63ost\x18\x04 \x01(\x01\x12\x12\n\nlowerBound\x18\x05 \x01(\x01\x12\x12\n\nupperBound\x18\x06 \x01(\x01\x12\x15\n\runblendedCost\x18\x07 \x01(\x01\x12\x1b\n\x13unblendedLowerBound\x18\x08 \x01(\x01\x12\x1b\n\x13unblendedUpperBound\x18\t \x01(\x01\x42j\n\x1f\x63loud.alphaus.blueapi.api.coverB\x19\x41piCoverCostForecastProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'api.cover.costforecast_pb2', globals())
