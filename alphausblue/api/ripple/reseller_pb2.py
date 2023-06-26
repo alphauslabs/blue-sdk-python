95115f4c0041189a30c89df7516448f4b852ceb2
mobingideployer <dev@mobingi.com>
2023-06-26 01:47:37 +0000

    refs/heads/main c47f51656ab56c5b696493ea9d6049325fbbd203

--- alphausblue/api/ripple/reseller_pb2.py
+++ alphausblue/api/ripple/reseller_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/ripple/reseller.proto\x12\x12\x62lueapi.api.ripple\"\x89\x02\n\x08Reseller\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x19\n\x11\x62illingInternalId\x18\x03 \x01(\t\x12\x16\n\x0e\x62illingGroupId\x18\x04 \x01(\t\x12\x18\n\x10\x62illingGroupName\x18\x05 \x01(\t\x12\x11\n\tgroupType\x18\n \x01(\t\x12\x12\n\nwaveStatus\x18\x06 \x01(\t\x12\x36\n\nwaveConfig\x18\x07 \x03(\x0b\x32\".blueapi.api.ripple.ResellerConfig\x12\x36\n\naquaConfig\x18\x08 \x03(\x0b\x32\".blueapi.api.ripple.ResellerConfig\",\n\x0eResellerConfig\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x08\x42i\n cloud.alphaus.blueapi.api.rippleB\x16\x41piRippleResellerProtoZ-github.com/alphauslabs/blue-sdk-go/api/rippleb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/ripple/reseller.proto\x12\x12\x62lueapi.api.ripple\"\xe0\x01\n\x08Reseller\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x0f\n\x07groupId\x18\x03 \x01(\t\x12\x11\n\tgroupName\x18\x04 \x01(\t\x12\x11\n\tgroupType\x18\x05 \x01(\t\x12\x12\n\nwaveStatus\x18\x06 \x01(\t\x12\x36\n\nwaveConfig\x18\x07 \x03(\x0b\x32\".blueapi.api.ripple.ResellerConfig\x12\x36\n\naquaConfig\x18\x08 \x03(\x0b\x32\".blueapi.api.ripple.ResellerConfig\",\n\x0eResellerConfig\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x08\x42i\n cloud.alphaus.blueapi.api.rippleB\x16\x41piRippleResellerProtoZ-github.com/alphauslabs/blue-sdk-go/api/rippleb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -23,7 +23,7 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'\n cloud.alphaus.blueapi.api.rippleB\026ApiRippleResellerProtoZ-github.com/alphauslabs/blue-sdk-go/api/ripple'
   _globals['_RESELLER']._serialized_start=50
-  _globals['_RESELLER']._serialized_end=315
-  _globals['_RESELLERCONFIG']._serialized_start=317
-  _globals['_RESELLERCONFIG']._serialized_end=361
+  _globals['_RESELLER']._serialized_end=274
+  _globals['_RESELLERCONFIG']._serialized_start=276
+  _globals['_RESELLERCONFIG']._serialized_end=320
 # @@protoc_insertion_point(module_scope)
