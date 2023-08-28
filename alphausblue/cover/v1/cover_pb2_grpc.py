8020d92d2888bfb0c3c534d18cd5fcbb58c2ed9a
mobingideployer <dev@mobingi.com>
2023-08-28 03:28:09 +0000

    refs/heads/main 68ab0f8a712b4f7512ecf51cd7d37fd7a3fd4144

--- alphausblue/cover/v1/cover_pb2_grpc.py
+++ alphausblue/cover/v1/cover_pb2_grpc.py
@@ -487,6 +487,11 @@ class CoverStub(object):
                 request_serializer=cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootRequest.SerializeToString,
                 response_deserializer=cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootResponse.FromString,
                 )
+        self.AddPartnerCenterCredentials = channel.unary_unary(
+                '/blueapi.cover.v1.Cover/AddPartnerCenterCredentials',
+                request_serializer=cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsRequest.SerializeToString,
+                response_deserializer=cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsResponse.FromString,
+                )
 
 
 class CoverServicer(object):
@@ -1150,6 +1155,13 @@ class CoverServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def AddPartnerCenterCredentials(self, request, context):
+        """WORK-IN-PROGRESS: PartnerCenterCredentials for Azure
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_CoverServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -1622,6 +1634,11 @@ def add_CoverServicer_to_server(servicer, server):
                     servicer.AddUserFromAuth0asRoot,
                     request_deserializer=cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootRequest.FromString,
                     response_serializer=cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootResponse.SerializeToString,
+            ),
+            'AddPartnerCenterCredentials': grpc.unary_unary_rpc_method_handler(
+                    servicer.AddPartnerCenterCredentials,
+                    request_deserializer=cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsRequest.FromString,
+                    response_serializer=cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -3229,5 +3246,22 @@ class Cover(object):
         return grpc.experimental.unary_unary(request, target, '/blueapi.cover.v1.Cover/AddUserFromAuth0asRoot',
             cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootRequest.SerializeToString,
             cover_dot_v1_dot_cover__pb2.AddUserFromAuth0asRootResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def AddPartnerCenterCredentials(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/blueapi.cover.v1.Cover/AddPartnerCenterCredentials',
+            cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsRequest.SerializeToString,
+            cover_dot_v1_dot_cover__pb2.AddPartnerCenterCredentialsResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
