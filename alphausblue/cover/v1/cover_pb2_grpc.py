a109fddc923c77bae9ce448476d92b98cf6eb65f
mobingideployer <dev@mobingi.com>
2023-10-16 12:45:01 +0000

    refs/heads/main d77ca1c7fe44f58845c3a71a616422cd8dec9ddf

--- alphausblue/cover/v1/cover_pb2_grpc.py
+++ alphausblue/cover/v1/cover_pb2_grpc.py
@@ -552,6 +552,11 @@ class CoverStub(object):
                 request_serializer=cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsRequest.SerializeToString,
                 response_deserializer=cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsResponse.FromString,
                 )
+        self.GetRiSpRecommendations = channel.unary_unary(
+                '/blueapi.cover.v1.Cover/GetRiSpRecommendations',
+                request_serializer=cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.SerializeToString,
+                response_deserializer=cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.FromString,
+                )
 
 
 class CoverServicer(object):
@@ -1306,6 +1311,13 @@ class CoverServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetRiSpRecommendations(self, request, context):
+        """Get the RI or SP recommendations for every account in a cost group
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_CoverServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -1843,6 +1855,11 @@ def add_CoverServicer_to_server(servicer, server):
                     servicer.UpdateChannelDetails,
                     request_deserializer=cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsRequest.FromString,
                     response_serializer=cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsResponse.SerializeToString,
+            ),
+            'GetRiSpRecommendations': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetRiSpRecommendations,
+                    request_deserializer=cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.FromString,
+                    response_serializer=cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -3671,5 +3688,22 @@ class Cover(object):
         return grpc.experimental.unary_unary(request, target, '/blueapi.cover.v1.Cover/UpdateChannelDetails',
             cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsRequest.SerializeToString,
             cover_dot_v1_dot_cover__pb2.UpdateChannelDetailsResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetRiSpRecommendations(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/blueapi.cover.v1.Cover/GetRiSpRecommendations',
+            cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.SerializeToString,
+            cover_dot_v1_dot_cover__pb2.GetRiSpRecommendationsRequest.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
