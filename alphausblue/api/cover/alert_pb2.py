06becfcbb38b98773d2882714e013d9ce846dd76
mobingideployer <dev@mobingi.com>
2023-11-07 02:05:20 +0000

    refs/heads/main a3274ab5e606ba68290afee87de2e2b3f1ec36d9

--- alphausblue/api/cover/alert_pb2.py
+++ alphausblue/api/cover/alert_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x61pi/cover/alert.proto\x12\x11\x62lueapi.api.cover\"\xe4\x01\n\tAlertData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0bgranularity\x18\x03 \x01(\t\x12\x12\n\ncostGroups\x18\x04 \x03(\t\x12\x32\n\x08\x63hannels\x18\x05 \x01(\x0b\x32 .blueapi.api.cover.AlertChannels\x12\x11\n\tcreatedBy\x18\x06 \x01(\t\x12\x11\n\tcreatedAt\x18\x07 \x01(\t\x12\x11\n\tupdatedAt\x18\x08 \x01(\t\x12\x13\n\x0b\x66ixedAmount\x18\t \x01(\x02\x12\x12\n\npercentage\x18\n \x01(\x02\"I\n\x0b\x43hannelData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04type\x18\x03 \x01(\t\x12\x12\n\nwebhookUrl\x18\x04 \x01(\t\">\n\rAlertChannels\x12\r\n\x05\x65mail\x18\x01 \x03(\t\x12\r\n\x05slack\x18\x02 \x03(\t\x12\x0f\n\x07msteams\x18\x03 \x03(\tBc\n\x1f\x63loud.alphaus.blueapi.api.coverB\x12\x41piCoverAlertProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x15\x61pi/cover/alert.proto\x12\x11\x62lueapi.api.cover\"\xe4\x01\n\tAlertData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0bgranularity\x18\x03 \x01(\t\x12\x12\n\ncostGroups\x18\x04 \x03(\t\x12\x32\n\x08\x63hannels\x18\x05 \x01(\x0b\x32 .blueapi.api.cover.AlertChannels\x12\x11\n\tcreatedBy\x18\x06 \x01(\t\x12\x11\n\tcreatedAt\x18\x07 \x01(\t\x12\x11\n\tupdatedAt\x18\x08 \x01(\t\x12\x13\n\x0b\x66ixedAmount\x18\t \x01(\x02\x12\x12\n\npercentage\x18\n \x01(\x02\"I\n\x0b\x43hannelData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04type\x18\x03 \x01(\t\x12\x12\n\nwebhookUrl\x18\x04 \x01(\t\"\xa1\x01\n\rAlertChannels\x12.\n\x05\x65mail\x18\x01 \x03(\x0b\x32\x1f.blueapi.api.cover.AlertChannel\x12.\n\x05slack\x18\x02 \x03(\x0b\x32\x1f.blueapi.api.cover.AlertChannel\x12\x30\n\x07msteams\x18\x03 \x03(\x0b\x32\x1f.blueapi.api.cover.AlertChannel\"(\n\x0c\x41lertChannel\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\tBc\n\x1f\x63loud.alphaus.blueapi.api.coverB\x12\x41piCoverAlertProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -25,6 +25,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_ALERTDATA']._serialized_end=273
   _globals['_CHANNELDATA']._serialized_start=275
   _globals['_CHANNELDATA']._serialized_end=348
-  _globals['_ALERTCHANNELS']._serialized_start=350
-  _globals['_ALERTCHANNELS']._serialized_end=412
+  _globals['_ALERTCHANNELS']._serialized_start=351
+  _globals['_ALERTCHANNELS']._serialized_end=512
+  _globals['_ALERTCHANNEL']._serialized_start=514
+  _globals['_ALERTCHANNEL']._serialized_end=554
 # @@protoc_insertion_point(module_scope)
