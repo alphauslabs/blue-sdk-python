fa661d95209d3d54e29e768dc82129c5a5fa4c62
mobingideployer <dev@mobingi.com>
2023-12-05 07:50:39 +0000

    refs/heads/main ba5c29a5da8c1e1e2a512b059991c9e63bf88573

--- alphausblue/api/cover/attribute_pb2.py
+++ alphausblue/api/cover/attribute_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/cover/attribute.proto\x12\x11\x62lueapi.api.cover\"*\n\x08\x43\x61tegory\x12\x0f\n\x07\x64isplay\x18\x01 \x01(\t\x12\r\n\x05query\x18\x02 \x01(\tBg\n\x1f\x63loud.alphaus.blueapi.api.coverB\x16\x41piCoverAttributeProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/cover/attribute.proto\x12\x11\x62lueapi.api.cover\"*\n\x08\x43\x61tegory\x12\x0f\n\x07\x64isplay\x18\x01 \x01(\t\x12\r\n\x05query\x18\x02 \x01(\t\"\"\n\x06Result\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\tBg\n\x1f\x63loud.alphaus.blueapi.api.coverB\x16\x41piCoverAttributeProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -23,4 +23,6 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._serialized_options = b'\n\037cloud.alphaus.blueapi.api.coverB\026ApiCoverAttributeProtoZ,github.com/alphauslabs/blue-sdk-go/api/cover'
   _globals['_CATEGORY']._serialized_start=48
   _globals['_CATEGORY']._serialized_end=90
+  _globals['_RESULT']._serialized_start=92
+  _globals['_RESULT']._serialized_end=126
 # @@protoc_insertion_point(module_scope)
