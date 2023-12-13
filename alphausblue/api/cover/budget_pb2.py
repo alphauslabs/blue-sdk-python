a5edd335a279bcb7c5dafd2f6fb1819de5860fa7
mobingideployer <dev@mobingi.com>
2023-12-13 04:35:58 +0000

    refs/heads/main e7446c7d7d8e630a55dac71335383d523a370ee9

--- alphausblue/api/cover/budget_pb2.py
+++ alphausblue/api/cover/budget_pb2.py
@@ -15,7 +15,7 @@ _sym_db = _symbol_database.Default()
 from alphausblue.api.cover import alert_pb2 as api_dot_cover_dot_alert__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x61pi/cover/budget.proto\x12\x11\x62lueapi.api.cover\x1a\x15\x61pi/cover/alert.proto\"\xb2\x02\n\nBudgetData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x13\n\x0bgranularity\x18\x04 \x01(\t\x12\x11\n\tstartDate\x18\x05 \x01(\t\x12\x0f\n\x07\x65ndDate\x18\x06 \x01(\t\x12\x13\n\x0btotalBudget\x18\x07 \x01(\x02\x12=\n\x0egranularBudget\x18\x08 \x03(\x0b\x32%.blueapi.api.cover.GranularBudgetData\x12\x34\n\tcostGroup\x18\t \x01(\x0b\x32!.blueapi.api.cover.AlertCostGroup\x12\x32\n\x08\x63hannels\x18\n \x01(\x0b\x32 .blueapi.api.cover.AlertChannels\"4\n\x12GranularBudgetData\x12\x0e\n\x06period\x18\x01 \x01(\t\x12\x0e\n\x06\x62udget\x18\x02 \x01(\x02\x42\x64\n\x1f\x63loud.alphaus.blueapi.api.coverB\x13\x41piCoverBudgetProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x16\x61pi/cover/budget.proto\x12\x11\x62lueapi.api.cover\x1a\x15\x61pi/cover/alert.proto\"\x82\x03\n\nBudgetData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x13\n\x0bgranularity\x18\x04 \x01(\t\x12\x11\n\tstartDate\x18\x05 \x01(\t\x12\x0f\n\x07\x65ndDate\x18\x06 \x01(\t\x12\x13\n\x0btotalBudget\x18\x07 \x01(\x02\x12=\n\x0egranularBudget\x18\x08 \x03(\x0b\x32%.blueapi.api.cover.GranularBudgetData\x12\x34\n\tcostGroup\x18\t \x01(\x0b\x32!.blueapi.api.cover.AlertCostGroup\x12\x32\n\x08\x63hannels\x18\n \x01(\x0b\x32 .blueapi.api.cover.AlertChannels\x12/\n\tthreshold\x18\x0b \x01(\x0b\x32\x1c.blueapi.api.cover.Threshold\x12\x0e\n\x06repeat\x18\x0c \x01(\x08\x12\r\n\x05\x64raft\x18\r \x01(\x08\"4\n\x12GranularBudgetData\x12\x0e\n\x06period\x18\x01 \x01(\t\x12\x0e\n\x06\x62udget\x18\x02 \x01(\x02\"4\n\tThreshold\x12\x13\n\x0b\x65xactAmount\x18\x01 \x03(\x02\x12\x12\n\npercentage\x18\x02 \x03(\x02\x42\x64\n\x1f\x63loud.alphaus.blueapi.api.coverB\x13\x41piCoverBudgetProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -24,7 +24,9 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['DESCRIPTOR']._options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n\037cloud.alphaus.blueapi.api.coverB\023ApiCoverBudgetProtoZ,github.com/alphauslabs/blue-sdk-go/api/cover'
   _globals['_BUDGETDATA']._serialized_start=69
-  _globals['_BUDGETDATA']._serialized_end=375
-  _globals['_GRANULARBUDGETDATA']._serialized_start=377
-  _globals['_GRANULARBUDGETDATA']._serialized_end=429
+  _globals['_BUDGETDATA']._serialized_end=455
+  _globals['_GRANULARBUDGETDATA']._serialized_start=457
+  _globals['_GRANULARBUDGETDATA']._serialized_end=509
+  _globals['_THRESHOLD']._serialized_start=511
+  _globals['_THRESHOLD']._serialized_end=563
 # @@protoc_insertion_point(module_scope)
