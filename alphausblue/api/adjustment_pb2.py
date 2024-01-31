009c5f4178d9bf86262d89451e274308572c541b
mobingideployer <dev@mobingi.com>
2024-01-31 07:30:15 +0000

    refs/heads/main 6531485209adeccb9c9c0265e68e5c523de08ed8

--- alphausblue/api/adjustment_pb2.py
+++ alphausblue/api/adjustment_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x61pi/adjustment.proto\x12\x0b\x62lueapi.api\"p\n\x10\x41\x64justmentConfig\x12*\n\x06\x63onfig\x18\x01 \x03(\x0b\x32\x1a.blueapi.api.ConfigFilters\x12\x30\n\x08\x61\x63\x63ounts\x18\x02 \x03(\x0b\x32\x1e.blueapi.api.ManagementAccount\"\\\n\x11ManagementAccount\x12\x1b\n\x13managementAccountId\x18\x01 \x01(\t\x12*\n\x06\x63onfig\x18\x02 \x03(\x0b\x32\x1a.blueapi.api.ConfigFilters\"\x82\x01\n\rConfigFilters\x12>\n\nandFilters\x18\x01 \x03(\x0b\x32*.blueapi.api.ConfigFilters.AndFiltersEntry\x1a\x31\n\x0f\x41ndFiltersEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42W\n\x19\x63loud.alphaus.blueapi.apiB\x12\x41piAdjustmentProtoZ&github.com/alphauslabs/blue-sdk-go/apib\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x14\x61pi/adjustment.proto\x12\x0b\x62lueapi.api\"\x80\x01\n\x10\x41\x64justmentConfig\x12*\n\x06\x63onfig\x18\x01 \x03(\x0b\x32\x1a.blueapi.api.ConfigFilters\x12\x0e\n\x06vendor\x18\x02 \x01(\t\x12\x30\n\x08\x61\x63\x63ounts\x18\x03 \x03(\x0b\x32\x1e.blueapi.api.ManagementAccount\"\\\n\x11ManagementAccount\x12\x1b\n\x13managementAccountId\x18\x01 \x01(\t\x12*\n\x06\x63onfig\x18\x02 \x03(\x0b\x32\x1a.blueapi.api.ConfigFilters\"\x82\x01\n\rConfigFilters\x12>\n\nandFilters\x18\x01 \x03(\x0b\x32*.blueapi.api.ConfigFilters.AndFiltersEntry\x1a\x31\n\x0f\x41ndFiltersEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42W\n\x19\x63loud.alphaus.blueapi.apiB\x12\x41piAdjustmentProtoZ&github.com/alphauslabs/blue-sdk-go/apib\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -24,12 +24,12 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['DESCRIPTOR']._serialized_options = b'\n\031cloud.alphaus.blueapi.apiB\022ApiAdjustmentProtoZ&github.com/alphauslabs/blue-sdk-go/api'
   _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._options = None
   _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._serialized_options = b'8\001'
-  _globals['_ADJUSTMENTCONFIG']._serialized_start=37
-  _globals['_ADJUSTMENTCONFIG']._serialized_end=149
-  _globals['_MANAGEMENTACCOUNT']._serialized_start=151
-  _globals['_MANAGEMENTACCOUNT']._serialized_end=243
-  _globals['_CONFIGFILTERS']._serialized_start=246
-  _globals['_CONFIGFILTERS']._serialized_end=376
-  _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._serialized_start=327
-  _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._serialized_end=376
+  _globals['_ADJUSTMENTCONFIG']._serialized_start=38
+  _globals['_ADJUSTMENTCONFIG']._serialized_end=166
+  _globals['_MANAGEMENTACCOUNT']._serialized_start=168
+  _globals['_MANAGEMENTACCOUNT']._serialized_end=260
+  _globals['_CONFIGFILTERS']._serialized_start=263
+  _globals['_CONFIGFILTERS']._serialized_end=393
+  _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._serialized_start=344
+  _globals['_CONFIGFILTERS_ANDFILTERSENTRY']._serialized_end=393
 # @@protoc_insertion_point(module_scope)
