2246a0140d96e454f390b934c807d9e9deede1ac
mobingideployer <dev@mobingi.com>
2024-07-18 09:54:27 +0000

    refs/heads/main 6cd8808f0b01fae19dde6e38435b9b4119bb99e8

--- alphausblue/admin/v1/admin_pb2_grpc.py
+++ alphausblue/admin/v1/admin_pb2_grpc.py
@@ -9,10 +9,10 @@ from alphausblue.api import notification_pb2 as api_dot_notification__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from protos import operation_pb2 as protos_dot_operation__pb2
 
-GRPC_GENERATED_VERSION = '1.64.1'
+GRPC_GENERATED_VERSION = '1.65.1'
 GRPC_VERSION = grpc.__version__
-EXPECTED_ERROR_RELEASE = '1.65.0'
-SCHEDULED_RELEASE_DATE = 'June 25, 2024'
+EXPECTED_ERROR_RELEASE = '1.66.0'
+SCHEDULED_RELEASE_DATE = 'August 6, 2024'
 _version_not_supported = False
 
 try:
--- alphausblue/operations/v1/operations_pb2_grpc.py
+++ alphausblue/operations/v1/operations_pb2_grpc.py
@@ -7,10 +7,10 @@ from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from alphausblue.operations.v1 import operations_pb2 as operations_dot_v1_dot_operations__pb2
 from protos import operation_pb2 as protos_dot_operation__pb2
 
-GRPC_GENERATED_VERSION = '1.64.1'
+GRPC_GENERATED_VERSION = '1.65.1'
 GRPC_VERSION = grpc.__version__
-EXPECTED_ERROR_RELEASE = '1.65.0'
-SCHEDULED_RELEASE_DATE = 'June 25, 2024'
+EXPECTED_ERROR_RELEASE = '1.66.0'
+SCHEDULED_RELEASE_DATE = 'August 6, 2024'
 _version_not_supported = False
 
 try:
