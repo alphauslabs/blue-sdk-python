9fef98c775d64868dc299d1fc3fea5030cd98897
mobingideployer <dev@mobingi.com>
2024-08-13 01:03:48 +0000

    refs/heads/main c8089f8460ddc8ffb24d674b8dfb7406e4039c5b

--- alphausblue/api/pricing/details_pb2.py
+++ alphausblue/api/pricing/details_pb2.py
@@ -12,9 +12,10 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/pricing/details.proto\x12\x13\x62lueapi.api.pricing\"\x94\x02\n\x0bPricingData\x12\x0e\n\x06vendor\x18\x01 \x01(\t\x12\x0f\n\x07service\x18\x02 \x01(\t\x12\x12\n\nregionCode\x18\x03 \x01(\t\x12\x0b\n\x03sku\x18\x04 \x01(\t\x12\x0c\n\x04unit\x18\x05 \x01(\t\x12\x14\n\x0cpricePerUnit\x18\x06 \x01(\x01\x12L\n\x0eserviceDetails\x18\x07 \x03(\x0b\x32\x34.blueapi.api.pricing.PricingData.ServiceDetailsEntry\x1aQ\n\x13ServiceDetailsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12)\n\x05value\x18\x02 \x01(\x0b\x32\x1a.blueapi.api.pricing.Value:\x02\x38\x01\"6\n\x05Value\x12\x10\n\x06strVal\x18\x01 \x01(\tH\x00\x12\x12\n\x08\x66loatVal\x18\x02 \x01(\x02H\x00\x42\x07\n\x05valueBk\n!cloud.alphaus.blueapi.api.pricingB\x16\x41piPricingDetailsProtoZ.github.com/alphauslabs/blue-sdk-go/api/pricingb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x19\x61pi/pricing/details.proto\x12\x13\x62lueapi.api.pricing\x1a\x1cgoogle/protobuf/struct.proto\"\xa4\x01\n\x0bPricingData\x12\x0e\n\x06vendor\x18\x01 \x01(\t\x12\x0f\n\x07service\x18\x02 \x01(\t\x12\x12\n\nregionCode\x18\x03 \x01(\t\x12\x0b\n\x03sku\x18\x04 \x01(\t\x12\x0c\n\x04unit\x18\x05 \x01(\t\x12\x14\n\x0cpricePerUnit\x18\x06 \x01(\x01\x12/\n\x0eserviceDetails\x18\x07 \x01(\x0b\x32\x17.google.protobuf.StructBk\n!cloud.alphaus.blueapi.api.pricingB\x16\x41piPricingDetailsProtoZ.github.com/alphauslabs/blue-sdk-go/api/pricingb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,12 +23,6 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'api.pricing.details_pb2', _
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n!cloud.alphaus.blueapi.api.pricingB\026ApiPricingDetailsProtoZ.github.com/alphauslabs/blue-sdk-go/api/pricing'
-  _globals['_PRICINGDATA_SERVICEDETAILSENTRY']._loaded_options = None
-  _globals['_PRICINGDATA_SERVICEDETAILSENTRY']._serialized_options = b'8\001'
-  _globals['_PRICINGDATA']._serialized_start=51
-  _globals['_PRICINGDATA']._serialized_end=327
-  _globals['_PRICINGDATA_SERVICEDETAILSENTRY']._serialized_start=246
-  _globals['_PRICINGDATA_SERVICEDETAILSENTRY']._serialized_end=327
-  _globals['_VALUE']._serialized_start=329
-  _globals['_VALUE']._serialized_end=383
+  _globals['_PRICINGDATA']._serialized_start=81
+  _globals['_PRICINGDATA']._serialized_end=245
 # @@protoc_insertion_point(module_scope)
