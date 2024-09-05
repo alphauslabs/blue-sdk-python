29bce010968d833723127adda0180312e7fdf006
mobingideployer <dev@mobingi.com>
2024-09-05 09:21:09 +0000

    refs/heads/main 0963fad41443fae7d54d3c3ed9af90f85e31844c

--- alphausblue/api/cover/unitcost_pb2.py
+++ alphausblue/api/cover/unitcost_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18\x61pi/cover/unitcost.proto\x12\x11\x62lueapi.api.cover\"\xf4\x01\n\x0cUnitCostData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x10\n\x08unitName\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x41\n\tunitItems\x18\x04 \x03(\x0b\x32..blueapi.api.cover.UnitCostData.UnitItemsEntry\x12\x11\n\tcreatedBy\x18\x05 \x01(\t\x12\x12\n\ncreateTime\x18\x06 \x01(\t\x12\x15\n\rlastUpdatedAt\x18\x07 \x01(\t\x1a\x30\n\x0eUnitItemsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x01:\x02\x38\x01\x42\x61\n\x1f\x63loud.alphaus.blueapi.api.coverB\x10\x41piUnitCostProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x18\x61pi/cover/unitcost.proto\x12\x11\x62lueapi.api.cover\"\xaf\x01\n\x0cUnitCostData\x12\n\n\x02id\x18\x01 \x01(\t\x12\x10\n\x08unitName\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12.\n\tunitItems\x18\x04 \x03(\x0b\x32\x1b.blueapi.api.cover.UnitItem\x12\x11\n\tcreatedBy\x18\x05 \x01(\t\x12\x12\n\ncreateTime\x18\x06 \x01(\t\x12\x15\n\rlastUpdatedAt\x18\x07 \x01(\t\"}\n\x08UnitItem\x12\x10\n\x08itemName\x18\x01 \x01(\t\x12\x14\n\x0c\x64istribution\x18\x02 \x01(\x01\x12I\n\x1d\x64\x65\x64icatedResourcesCombination\x18\x03 \x03(\x0b\x32\".blueapi.api.cover.ResourcesFilter\"\x8c\x01\n\x0fResourcesFilter\x12\x46\n\nandFilters\x18\x01 \x03(\x0b\x32\x32.blueapi.api.cover.ResourcesFilter.AndFiltersEntry\x1a\x31\n\x0f\x41ndFiltersEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x61\n\x1f\x63loud.alphaus.blueapi.api.coverB\x10\x41piUnitCostProtoZ,github.com/alphauslabs/blue-sdk-go/api/coverb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,10 +32,14 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'api.cover.unitcost_pb2', _g
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n\037cloud.alphaus.blueapi.api.coverB\020ApiUnitCostProtoZ,github.com/alphauslabs/blue-sdk-go/api/cover'
-  _globals['_UNITCOSTDATA_UNITITEMSENTRY']._loaded_options = None
-  _globals['_UNITCOSTDATA_UNITITEMSENTRY']._serialized_options = b'8\001'
+  _globals['_RESOURCESFILTER_ANDFILTERSENTRY']._loaded_options = None
+  _globals['_RESOURCESFILTER_ANDFILTERSENTRY']._serialized_options = b'8\001'
   _globals['_UNITCOSTDATA']._serialized_start=48
-  _globals['_UNITCOSTDATA']._serialized_end=292
-  _globals['_UNITCOSTDATA_UNITITEMSENTRY']._serialized_start=244
-  _globals['_UNITCOSTDATA_UNITITEMSENTRY']._serialized_end=292
+  _globals['_UNITCOSTDATA']._serialized_end=223
+  _globals['_UNITITEM']._serialized_start=225
+  _globals['_UNITITEM']._serialized_end=350
+  _globals['_RESOURCESFILTER']._serialized_start=353
+  _globals['_RESOURCESFILTER']._serialized_end=493
+  _globals['_RESOURCESFILTER_ANDFILTERSENTRY']._serialized_start=444
+  _globals['_RESOURCESFILTER_ANDFILTERSENTRY']._serialized_end=493
 # @@protoc_insertion_point(module_scope)
