eb9b1df59f4604e50fe9570f73d0b02734b3b37a
mobingideployer <dev@mobingi.com>
2024-11-08 05:01:48 +0000

    refs/heads/main 44ea3c091264feac64466fe5085ae5040d2a86b1

--- alphausblue/api/keyvalue_pb2.py
+++ alphausblue/api/keyvalue_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x61pi/keyvalue.proto\x12\x0b\x62lueapi.api\"&\n\x08KeyValue\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\tBU\n\x19\x63loud.alphaus.blueapi.apiB\x10\x41piKeyValueProtoZ&github.com/alphauslabs/blue-sdk-go/apib\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12\x61pi/keyvalue.proto\x12\x0b\x62lueapi.api\"&\n\x08KeyValue\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t\"O\n\x0bKeyValueMap\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x15\n\x0bstringValue\x18\x02 \x01(\tH\x00\x12\x13\n\tboolValue\x18\x03 \x01(\x08H\x00\x42\x07\n\x05valueBU\n\x19\x63loud.alphaus.blueapi.apiB\x10\x41piKeyValueProtoZ&github.com/alphauslabs/blue-sdk-go/apib\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -34,4 +34,6 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._serialized_options = b'\n\031cloud.alphaus.blueapi.apiB\020ApiKeyValueProtoZ&github.com/alphauslabs/blue-sdk-go/api'
   _globals['_KEYVALUE']._serialized_start=35
   _globals['_KEYVALUE']._serialized_end=73
+  _globals['_KEYVALUEMAP']._serialized_start=75
+  _globals['_KEYVALUEMAP']._serialized_end=154
 # @@protoc_insertion_point(module_scope)
--- alphausblue/cost/v1/cost_pb2_grpc.py
+++ alphausblue/cost/v1/cost_pb2_grpc.py
@@ -448,6 +448,7 @@ class CostServicer(object):
 
     def ReadAccountOriginalResources(self, request, context):
         """WORK-IN-PROGRESS: Reads all vendor account-original-resources.
+        Account that have not been used for one month will no longer be retrieved.
         """
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
