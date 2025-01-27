d3f4c6ae85a3597663bd1780a87286a39c6e5bb2
mobingideployer <dev@mobingi.com>
2025-01-27 05:21:45 +0000

    refs/heads/main 573391870205078ef7b3312436d2f8783989bab9

--- alphausblue/operations/v1/operations_pb2_grpc.py
+++ alphausblue/operations/v1/operations_pb2_grpc.py
@@ -7,7 +7,7 @@ from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from alphausblue.operations.v1 import operations_pb2 as operations_dot_v1_dot_operations__pb2
 from protos import operation_pb2 as protos_dot_operation__pb2
 
-GRPC_GENERATED_VERSION = '1.69.0'
+GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
--- alphausblue/pricing/v1/pricing_pb2_grpc.py
+++ alphausblue/pricing/v1/pricing_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from alphausblue.pricing.v1 import pricing_pb2 as pricing_dot_v1_dot_pricing__pb2
 
-GRPC_GENERATED_VERSION = '1.69.0'
+GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
