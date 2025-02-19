2f7834c7df99bd81b50fb99b18a08915dc0f8902
mobingideployer <dev@mobingi.com>
2025-02-19 06:48:59 +0000

    refs/heads/main 9254f11aacedfedbdcc75d2341eed968594e1077

--- alphausblue/billing/v1/billing_pb2_grpc.py
+++ alphausblue/billing/v1/billing_pb2_grpc.py
@@ -439,6 +439,11 @@ class BillingStub(object):
                 request_serializer=billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvRequest.SerializeToString,
                 response_deserializer=billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvResponse.FromString,
                 _registered_method=True)
+        self.UpdateBillingGroupBasicInformation = channel.unary_unary(
+                '/blueapi.billing.v1.Billing/UpdateBillingGroupBasicInformation',
+                request_serializer=billing_dot_v1_dot_billing__pb2.UpdateBillingGroupBasicInformationRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                _registered_method=True)
 
 
 class BillingServicer(object):
@@ -1009,6 +1014,13 @@ class BillingServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def UpdateBillingGroupBasicInformation(self, request, context):
+        """Update billing group basic information
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_BillingServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -1406,6 +1418,11 @@ def add_BillingServicer_to_server(servicer, server):
                     servicer.ExportInvoiceSettingCsv,
                     request_deserializer=billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvRequest.FromString,
                     response_serializer=billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvResponse.SerializeToString,
+            ),
+            'UpdateBillingGroupBasicInformation': grpc.unary_unary_rpc_method_handler(
+                    servicer.UpdateBillingGroupBasicInformation,
+                    request_deserializer=billing_dot_v1_dot_billing__pb2.UpdateBillingGroupBasicInformationRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -3542,6 +3559,33 @@ class Billing(object):
             '/blueapi.billing.v1.Billing/ExportInvoiceSettingCsv',
             billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvRequest.SerializeToString,
             billing_dot_v1_dot_billing__pb2.ExportInvoiceSettingCsvResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def UpdateBillingGroupBasicInformation(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/blueapi.billing.v1.Billing/UpdateBillingGroupBasicInformation',
+            billing_dot_v1_dot_billing__pb2.UpdateBillingGroupBasicInformationRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
             options,
             channel_credentials,
             insecure,
