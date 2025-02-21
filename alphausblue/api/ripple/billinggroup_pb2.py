0bd22d0d368bd2a865b27e5b96c90b4988573a2f
mobingideployer <dev@mobingi.com>
2025-02-21 01:23:10 +0000

    refs/heads/main 49fe01236f39ef8bdc825a4b6656c7d3e0ac3d60

--- alphausblue/api/ripple/billinggroup_pb2.py
+++ alphausblue/api/ripple/billinggroup_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1d\x61pi/ripple/billinggroup.proto\x12\x12\x62lueapi.api.ripple\"\xb7\x02\n\x10\x42illingGroupInfo\x12\x14\n\x0c\x62illingTitle\x18\x01 \x01(\t\x12\x13\n\x0bphoneNumber\x18\x02 \x01(\t\x12\x12\n\npostalCode\x18\x03 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x04 \x01(\t\x12\x10\n\x08personal\x18\x05 \x01(\t\x12\x0f\n\x07remarks\x18\x06 \x01(\t\x12\x11\n\tprojectId\x18\x07 \x01(\t\x12\x10\n\x08language\x18\x08 \x01(\t\x12\x13\n\x0b\x64isplayCost\x18\t \x01(\t\x12\x18\n\x10\x65xchangeRateType\x18\n \x01(\t\x12\x0e\n\x06qrCode\x18\x0b \x01(\x08\x12\x19\n\x11invoiceTemplateId\x18\x0c \x01(\t\x12\x0c\n\x04\x63ity\x18\r \x01(\t\x12\x12\n\nprefecture\x18\x0e \x01(\t\x12\x0f\n\x07\x63ountry\x18\x0f \x01(\tBq\n cloud.alphaus.blueapi.api.rippleB\x1e\x41piRippleBillingGroupInfoProtoZ-github.com/alphauslabs/blue-sdk-go/api/rippleb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1d\x61pi/ripple/billinggroup.proto\x12\x12\x62lueapi.api.ripple\"\xf2\x02\n\x10\x42illingGroupInfo\x12\x14\n\x0c\x62illingTitle\x18\x01 \x01(\t\x12\x13\n\x0bphoneNumber\x18\x02 \x01(\t\x12\x12\n\npostalCode\x18\x03 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x04 \x01(\t\x12\x10\n\x08personal\x18\x05 \x01(\t\x12\x0f\n\x07remarks\x18\x06 \x01(\t\x12\x11\n\tprojectId\x18\x07 \x01(\t\x12\x10\n\x08language\x18\x08 \x01(\t\x12\x13\n\x0b\x64isplayCost\x18\t \x01(\t\x12\x18\n\x10\x65xchangeRateType\x18\n \x01(\t\x12\x0e\n\x06qrCode\x18\x0b \x01(\x08\x12\x19\n\x11invoiceTemplateId\x18\x0c \x01(\t\x12\x0c\n\x04\x63ity\x18\r \x01(\t\x12\x12\n\nprefecture\x18\x0e \x01(\t\x12\x0f\n\x07\x63ountry\x18\x0f \x01(\t\x12\r\n\x05state\x18\x10 \x01(\t\x12\x14\n\x0c\x61\x64\x64ressLine1\x18\x11 \x01(\t\x12\x14\n\x0c\x61\x64\x64ressLine2\x18\x12 \x01(\tBq\n cloud.alphaus.blueapi.api.rippleB\x1e\x41piRippleBillingGroupInfoProtoZ-github.com/alphauslabs/blue-sdk-go/api/rippleb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,5 +33,5 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'\n cloud.alphaus.blueapi.api.rippleB\036ApiRippleBillingGroupInfoProtoZ-github.com/alphauslabs/blue-sdk-go/api/ripple'
   _globals['_BILLINGGROUPINFO']._serialized_start=54
-  _globals['_BILLINGGROUPINFO']._serialized_end=365
+  _globals['_BILLINGGROUPINFO']._serialized_end=424
 # @@protoc_insertion_point(module_scope)
