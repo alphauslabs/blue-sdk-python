0510fd6534765f3f5ee60ef86b822ea1ddd1b031
mobingideployer <dev@mobingi.com>
2025-06-18 13:43:58 +0000

    refs/heads/main 247fa8aabad8c31b946105bbe7e9ac07c7080709

--- alphausblue/prism/v1/prism_pb2_grpc.py
+++ alphausblue/prism/v1/prism_pb2_grpc.py
@@ -131,6 +131,11 @@ class PrismStub(object):
                 request_serializer=prism_dot_v1_dot_prism__pb2.AssignProjectToTeamRequest.SerializeToString,
                 response_deserializer=prism_dot_v1_dot_prism__pb2.AssignProjectToTeamResponse.FromString,
                 _registered_method=True)
+        self.DeleteTeam = channel.unary_unary(
+                '/blueapi.prism.v1.Prism/DeleteTeam',
+                request_serializer=prism_dot_v1_dot_prism__pb2.DeleteTeamRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                _registered_method=True)
 
 
 class PrismServicer(object):
@@ -256,6 +261,12 @@ class PrismServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def DeleteTeam(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_PrismServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -353,6 +364,11 @@ def add_PrismServicer_to_server(servicer, server):
                     servicer.AssignProjectToTeam,
                     request_deserializer=prism_dot_v1_dot_prism__pb2.AssignProjectToTeamRequest.FromString,
                     response_serializer=prism_dot_v1_dot_prism__pb2.AssignProjectToTeamResponse.SerializeToString,
+            ),
+            'DeleteTeam': grpc.unary_unary_rpc_method_handler(
+                    servicer.DeleteTeam,
+                    request_deserializer=prism_dot_v1_dot_prism__pb2.DeleteTeamRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -869,6 +885,33 @@ class Prism(object):
             '/blueapi.prism.v1.Prism/AssignProjectToTeam',
             prism_dot_v1_dot_prism__pb2.AssignProjectToTeamRequest.SerializeToString,
             prism_dot_v1_dot_prism__pb2.AssignProjectToTeamResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def DeleteTeam(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/blueapi.prism.v1.Prism/DeleteTeam',
+            prism_dot_v1_dot_prism__pb2.DeleteTeamRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
             options,
             channel_credentials,
             insecure,
