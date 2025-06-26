a23d56fb23f104c36fda88d1a2f4bbf987172c1b
mobingideployer <dev@mobingi.com>
2025-06-26 01:27:53 +0000

    refs/heads/main 25bc22cc9f5bc18bad6ac19443c74b52fed16087

--- alphausblue/prism/v1/prism_pb2_grpc.py
+++ alphausblue/prism/v1/prism_pb2_grpc.py
@@ -181,6 +181,11 @@ class PrismStub(object):
                 request_serializer=prism_dot_v1_dot_prism__pb2.ListActivitiesRequest.SerializeToString,
                 response_deserializer=prism_dot_v1_dot_prism__pb2.Activity.FromString,
                 _registered_method=True)
+        self.JoinOrganization = channel.unary_unary(
+                '/blueapi.prism.v1.Prism/JoinOrganization',
+                request_serializer=prism_dot_v1_dot_prism__pb2.JoinOrganizationRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                _registered_method=True)
 
 
 class PrismServicer(object):
@@ -366,6 +371,12 @@ class PrismServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def JoinOrganization(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_PrismServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -513,6 +524,11 @@ def add_PrismServicer_to_server(servicer, server):
                     servicer.ListActivities,
                     request_deserializer=prism_dot_v1_dot_prism__pb2.ListActivitiesRequest.FromString,
                     response_serializer=prism_dot_v1_dot_prism__pb2.Activity.SerializeToString,
+            ),
+            'JoinOrganization': grpc.unary_unary_rpc_method_handler(
+                    servicer.JoinOrganization,
+                    request_deserializer=prism_dot_v1_dot_prism__pb2.JoinOrganizationRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -1299,6 +1315,33 @@ class Prism(object):
             '/blueapi.prism.v1.Prism/ListActivities',
             prism_dot_v1_dot_prism__pb2.ListActivitiesRequest.SerializeToString,
             prism_dot_v1_dot_prism__pb2.Activity.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def JoinOrganization(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/blueapi.prism.v1.Prism/JoinOrganization',
+            prism_dot_v1_dot_prism__pb2.JoinOrganizationRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
             options,
             channel_credentials,
             insecure,
