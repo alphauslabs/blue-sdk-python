c76c307c9cf3177afee0697f07b515009d50d1a2
mobingideployer <dev@mobingi.com>
2025-07-08 07:23:24 +0000

    refs/heads/main 9d9334246bb8dcd779ebb1a2d0a33312df7471f1

--- alphausblue/prism/v1/prism_pb2_grpc.py
+++ alphausblue/prism/v1/prism_pb2_grpc.py
@@ -191,6 +191,11 @@ class PrismStub(object):
                 request_serializer=prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeRequest.SerializeToString,
                 response_deserializer=prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeResponse.FromString,
                 _registered_method=True)
+        self.ShareGDrive = channel.unary_unary(
+                '/blueapi.prism.v1.Prism/ShareGDrive',
+                request_serializer=prism_dot_v1_dot_prism__pb2.ShareGDriveRequest.SerializeToString,
+                response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+                _registered_method=True)
 
 
 class PrismServicer(object):
@@ -388,6 +393,12 @@ class PrismServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def ShareGDrive(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_PrismServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -545,6 +556,11 @@ def add_PrismServicer_to_server(servicer, server):
                     servicer.GenerateOrganizationCode,
                     request_deserializer=prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeRequest.FromString,
                     response_serializer=prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeResponse.SerializeToString,
+            ),
+            'ShareGDrive': grpc.unary_unary_rpc_method_handler(
+                    servicer.ShareGDrive,
+                    request_deserializer=prism_dot_v1_dot_prism__pb2.ShareGDriveRequest.FromString,
+                    response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -1385,6 +1401,33 @@ class Prism(object):
             '/blueapi.prism.v1.Prism/GenerateOrganizationCode',
             prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeRequest.SerializeToString,
             prism_dot_v1_dot_prism__pb2.GenerateOrganizationCodeResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def ShareGDrive(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/blueapi.prism.v1.Prism/ShareGDrive',
+            prism_dot_v1_dot_prism__pb2.ShareGDriveRequest.SerializeToString,
+            google_dot_protobuf_dot_empty__pb2.Empty.FromString,
             options,
             channel_credentials,
             insecure,
