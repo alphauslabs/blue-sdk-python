bfa9b7e219aeb91dc8f595404e48f609f1642faa
mobingideployer <dev@mobingi.com>
2025-09-29 05:44:37 +0000

    refs/heads/main f9f413d339ee54866f16613a1823d5ecb94f2969

--- alphausblue/org/v1/org_pb2_grpc.py
+++ alphausblue/org/v1/org_pb2_grpc.py
@@ -7,7 +7,7 @@ from alphausblue.api.ripple import org_pb2 as api_dot_ripple_dot_org__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from alphausblue.org.v1 import org_pb2 as org_dot_v1_dot_org__pb2
 
-GRPC_GENERATED_VERSION = '1.75.0'
+GRPC_GENERATED_VERSION = '1.75.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
