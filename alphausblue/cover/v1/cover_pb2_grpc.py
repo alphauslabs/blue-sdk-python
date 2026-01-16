f6fd2722750f240facb24548e7663d79d5ae7be5
mobingideployer <dev@mobingi.com>
2026-01-16 06:05:17 +0000

    refs/heads/main 41c5358b6dd846ab1e625c4f733edb01a6e49d2e

--- alphausblue/cover/v1/cover_pb2_grpc.py
+++ alphausblue/cover/v1/cover_pb2_grpc.py
@@ -1012,6 +1012,11 @@ class CoverStub(object):
                 request_serializer=cover_dot_v1_dot_cover__pb2.ListRecommendationsV3Request.SerializeToString,
                 response_deserializer=cover_dot_v1_dot_cover__pb2.RecommendationSummary.FromString,
                 _registered_method=True)
+        self.ExecuteRecommendationV3 = channel.unary_unary(
+                '/blueapi.cover.v1.Cover/ExecuteRecommendationV3',
+                request_serializer=cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Request.SerializeToString,
+                response_deserializer=cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Response.FromString,
+                _registered_method=True)
 
 
 class CoverServicer(object):
@@ -2368,6 +2373,12 @@ class CoverServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def ExecuteRecommendationV3(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_CoverServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -3345,6 +3356,11 @@ def add_CoverServicer_to_server(servicer, server):
                     servicer.ListRecommendationsV3,
                     request_deserializer=cover_dot_v1_dot_cover__pb2.ListRecommendationsV3Request.FromString,
                     response_serializer=cover_dot_v1_dot_cover__pb2.RecommendationSummary.SerializeToString,
+            ),
+            'ExecuteRecommendationV3': grpc.unary_unary_rpc_method_handler(
+                    servicer.ExecuteRecommendationV3,
+                    request_deserializer=cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Request.FromString,
+                    response_serializer=cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Response.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -8613,6 +8629,33 @@ class Cover(object):
             '/blueapi.cover.v1.Cover/ListRecommendationsV3',
             cover_dot_v1_dot_cover__pb2.ListRecommendationsV3Request.SerializeToString,
             cover_dot_v1_dot_cover__pb2.RecommendationSummary.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def ExecuteRecommendationV3(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/blueapi.cover.v1.Cover/ExecuteRecommendationV3',
+            cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Request.SerializeToString,
+            cover_dot_v1_dot_cover__pb2.ExecuteRecommendationV3Response.FromString,
             options,
             channel_credentials,
             insecure,
