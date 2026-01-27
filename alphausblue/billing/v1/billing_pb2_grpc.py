daeed66765dfe068f1a40e0c6f4889da08b4f1f6
mobingideployer <dev@mobingi.com>
2026-01-27 06:09:06 +0000

    refs/heads/main 65fbda6f87671dd0a215916d4a5506e987972989

--- alphausblue/billing/v1/billing_pb2_grpc.py
+++ alphausblue/billing/v1/billing_pb2_grpc.py
@@ -640,6 +640,11 @@ class BillingStub(object):
                 request_serializer=billing_dot_v1_dot_billing__pb2.GetChildBillingGroupRequest.SerializeToString,
                 response_deserializer=billing_dot_v1_dot_billing__pb2.GetChildBillingGroupResponse.FromString,
                 _registered_method=True)
+        self.ListChildBillingGroups = channel.unary_stream(
+                '/blueapi.billing.v1.Billing/ListChildBillingGroups',
+                request_serializer=billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsRequest.SerializeToString,
+                response_deserializer=billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsResponse.FromString,
+                _registered_method=True)
 
 
 class BillingServicer(object):
@@ -1489,6 +1494,13 @@ class BillingServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def ListChildBillingGroups(self, request, context):
+        """Lists child billing groups under a specific filter
+        """
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_BillingServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -2086,6 +2098,11 @@ def add_BillingServicer_to_server(servicer, server):
                     servicer.GetChildBillingGroup,
                     request_deserializer=billing_dot_v1_dot_billing__pb2.GetChildBillingGroupRequest.FromString,
                     response_serializer=billing_dot_v1_dot_billing__pb2.GetChildBillingGroupResponse.SerializeToString,
+            ),
+            'ListChildBillingGroups': grpc.unary_stream_rpc_method_handler(
+                    servicer.ListChildBillingGroups,
+                    request_deserializer=billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsRequest.FromString,
+                    response_serializer=billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -5302,6 +5319,33 @@ class Billing(object):
             '/blueapi.billing.v1.Billing/GetChildBillingGroup',
             billing_dot_v1_dot_billing__pb2.GetChildBillingGroupRequest.SerializeToString,
             billing_dot_v1_dot_billing__pb2.GetChildBillingGroupResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def ListChildBillingGroups(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_stream(
+            request,
+            target,
+            '/blueapi.billing.v1.Billing/ListChildBillingGroups',
+            billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsRequest.SerializeToString,
+            billing_dot_v1_dot_billing__pb2.ListChildBillingGroupsResponse.FromString,
             options,
             channel_credentials,
             insecure,
